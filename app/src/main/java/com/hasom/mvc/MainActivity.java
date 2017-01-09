4dcfb392b9937f83a28355a36f56b88c11d8c9cc
hasom <heroljh80@gmail.com>
2017-01-09 12:42:05 +0900

    ㅇㅇ

--- app/src/main/java/com/hasom/mvc/MainActivity.java
+++ app/src/main/java/com/hasom/mvc/MainActivity.java
@@ -15,6 +15,9 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * create by junho.lee
+ */
 public class MainActivity extends AppCompatActivity implements IssueModel.ModelDataChange {
 
     @BindView(R.id.listView)
