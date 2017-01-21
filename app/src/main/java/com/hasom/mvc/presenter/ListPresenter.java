b9bc514a2d9bb211a107b2f417e1561be02a428a
hasom <heroljh80@gmail.com>
2017-01-21 21:49:50 +0900

    # Refresh Issue List  - SwipeRefreshLayout 적용
     # Issue List Paging 적용
    
     # Issue List CommentCount Update 적용
    
     # Issue Detail Body텍스트 추가

---
+++ app/src/main/java/com/hasom/mvc/presenter/ListPresenter.java
@@ -0,0 +1,8 @@
+package com.hasom.mvc.presenter;
+
+/**
+ * Created by leejunho on 2017. 1. 12..
+ */
+
+public interface ListPresenter {
+}
---
+++ app/src/main/java/com/hasom/mvc/presenter/ListPresenterImpl.java
@@ -0,0 +1,10 @@
+package com.hasom.mvc.presenter;
+
+import ListPresenter;
+
+/**
+ * Created by leejunho on 2017. 1. 12..
+ */
+
+public class ListPresenterImpl implements ListPresenter {
+}
